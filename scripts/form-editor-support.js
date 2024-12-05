452f1c99f3a708b9fef367d72a49c01c22bc6c4d
Navneet Agarwal <53942457+navneet-ag@users.noreply.github.com>
2024-12-05 12:02:31 +0530

    Adding extenstion

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -346,6 +346,15 @@ export function attachEventListners(main) {
   document.body.addEventListener('aue:ui-edit', ueEditModeHandler);
 }
 
+function enableExtension() {
+  const head = document.getElementsByTagName('head')[0];
+  const meta = document.createElement('meta');
+  meta.name = 'urn:adobe:aue:config:extensions';
+  meta.content = 'https://283250-415indigomongoose-stage.adobeio-static.net';
+  // meta.content = 'https://283250-684greenantelope-stage.adobeio-static.net';
+  head.appendChild(meta);
+}
+
 const observer = new MutationObserver(instrumentForms);
 observer.observe(document, { childList: true, subtree: true, attributeFilter: ['form'] });
 loadCSS(`${window.hlx.codeBasePath}/scripts/form-editor-support.css`);
